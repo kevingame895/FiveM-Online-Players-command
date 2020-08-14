3845ec32fa38a47c9da72d55b2d104b4109ec660
kevingame895 <64527537+kevingame895@users.noreply.github.com>
2020-08-14 09:38:11 +0200

    Update client.lua

--- pop/client/client.lua
+++ pop/client/client.lua
@@ -21,3 +21,17 @@ function addClientChat(message)
         args = {"Server", message} -- Change "Server" to whatever you want it to be 
     })
 end
+
+-- [Optional] This down here makes it show on keypress 
+-- Remove the "--"'s below if you want this to be working by keypress
+--Citizen.CreateThread(function()
+    --while true do
+        --Citizen.Wait(2)
+        --if IsControlJustReleased(1, 303) then -- Change 303 (U) to the keybind you want (https://docs.fivem.net/game-references/controls/)
+            --local message = 'There is currently ^1'..playerPop..'/'..serverLimit..' ^0players online!'
+            --playerPop = GetNumberOfPlayers()
+            --addClientChat(message)
+        --end     
+    --end
+    --Citizen.Wait(2)
+--end)
