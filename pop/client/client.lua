d777013c654c1f4c719d6bfacf97fd959e2f4007
kevingame895 <64527537+kevingame895@users.noreply.github.com>
2020-09-02 18:22:55 +0200

    Update client.lua

--- pop/client/client.lua
+++ pop/client/client.lua
@@ -1,8 +1,17 @@
-local playerPop = 0
-local serverLimit = 64 -- Change "64" to the amount of slots in your server
+--------------------------------------------------------------------------------------------------------------------------
+---------------------------- CFG ------ CFG ----- CFG -----------------------------------------------------------------------
 
-RegisterCommand('pop', function() -- Change 'pop' to the command you want 
-    local message = 'There is currently '..playerPop..'/'..serverLimit..' players online!'
+local serverLimit = 32 -- Change "32" to the amount of slots in your server
+local keybind = 303 -- Change 303 (U) to the keybind you want (https://docs.fivem.net/game-references/controls/)
+local command = 'pop' -- Change pop to the command you want 
+local ServerNanme = "Server" -- Change Server to the "server/console" name you want to type the message
+
+----------------------------- CFG ----- CFG ----- CFG ----------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
+local playerPop = 0
+
+RegisterCommand(command, function()
+    local message = 'There is currently ^1'..playerPop..'/'..serverLimit..' ^0players online!'
     addClientChat(message)
 end, false)
 
@@ -18,21 +27,20 @@ function addClientChat(message)
     TriggerEvent ('chat:addMessage', {
         color = {0, 255, 255}, -- Change the RGB colour code to the colour you want the "Server" text to be 
         multiline = true, 
-        args = {"Server", message} -- Change "Server" to whatever you want it to be 
+        args = {ServerNanme, message} -- Change "Server" to whatever you want it to be 
     })
 end
 
 -- [Optional] This down here makes it show on keypress 
 -- Remove the "--"'s below if you want this to be working by keypress
-
 --Citizen.CreateThread(function()
     --while true do
-        --Citizen.Wait(2)
-        --if IsControlJustReleased(1, 303) then -- Change 303 (U) to the keybind you want (https://docs.fivem.net/game-references/controls/)
+        --Citizen.Wait(0)
+        --if IsControlJustReleased(1, keybind) then
             --local message = 'There is currently ^1'..playerPop..'/'..serverLimit..' ^0players online!'
             --playerPop = GetNumberOfPlayers()
             --addClientChat(message)
         --end     
     --end
-    --Citizen.Wait(2)
+    --Citizen.Wait(0)
 --end)
